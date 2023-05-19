20bcb9850a5bf8fbf28b789df14b59d186f06ca2
gomminjae <gommj0611@naver.com>
2023-05-19 23:32:26 +0900

    Project Setting

--- Knocking/Sources/Home/HomeViewController.swift
+++ Knocking/Sources/Home/HomeViewController.swift
@@ -1,19 +1,21 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Knocking
 //
 //  Created by 권민재 on 2023/05/19.
 //
 
 import UIKit
+import RxSwift
+import SnapKit
+import RxCocoa
 
-class ViewController: UIViewController {
+class HomeViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
